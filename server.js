61e90a4508c1a101ff38f949924b071163fe33e4
Mayank-kumarSDE <mayankkumar8481@gmail.com>
2026-02-24 17:08:17 +0530

    final changes done

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ import pingRouter from './src/router/pingRouter.js'
 import genricError from './src/middleware/error.middleware.js';
 import logger from './src/config/logger.config.js';
 import { attachCorrelationIdMiddleware} from './src/middleware/corelation.middleware.js'
-const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(attachCorrelationIdMiddleware)
 app.use(pingRouter)
 
 app.use(genricError);
-app.listen(port, () => {
-  logger.info(`Example app listening `)
+app.listen(serverConfig.port, () => {
+  logger.info(`Example app listening on localhost ${serverConfig.port} `)
 })
